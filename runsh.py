5cccbdc40751dbb8cd5c506ef0bd8678aadb7c77
mary <dsa66253@yahoo.com.tw>
2022-11-29 23:28:51 +0800

    1127_2.brutL0L1

--- runsh.py
+++ runsh.py
@@ -71,7 +71,7 @@ def brutNas():
             manualAssign["layer_0_2"][i] = 1
             manualAssign["layer_2_5"][j] = 1
             f = setStdoutToFile("./curExperiment.json")
-            curExpName = "1126_2.brutL0L1.{}_{}".format(i, j)
+            curExpName = "1127_2.brutL0L1.{}_{}".format(i, j)
             desDir = join("./log", curExpName)
             print(json.dumps({curExpName:1}, indent=4))
             setStdoutToDefault(f)
