6edbf43b1cb9622393cdb14674fe2fba8a03fce9
Mary <dsa66253@yahoo.com.tw>
2022-12-20 21:48:35 +0800

    1216.brutL2L3

--- runsh.py
+++ runsh.py
@@ -45,13 +45,13 @@ def brutNas():
             0,
             0
         ],
-        # "layer_1_2": [
-        #     1,
-        #     0,
-        #     0,
-        #     0,
-        #     0
-        # ],
+        "layer_1_2": [
+            1,
+            0,
+            0,
+            0,
+            0
+        ],
         # "layer_2_3": [
         #     1,
         #     0,
@@ -66,7 +66,7 @@ def brutNas():
         #     0,
         #     0
         # ],
-        "layer_1_4": [
+        "layer_2_4": [
             0,
             0,
             0,
@@ -89,10 +89,10 @@ def brutNas():
             # for second layeer
             manualAssign = copy.deepcopy(initiManualAssign)
             
-            manualAssign["layer_1_4"][i] = 1
+            manualAssign["layer_2_4"][i] = 1
             manualAssign["layer_4_5"][j] = 1
             f = setStdoutToFile("./curExperiment.json")
-            curExpName = "1213.brutL1L2.{}_{}".format(i, j)
+            curExpName = "1216.brutL2L3.{}_{}".format(i, j)
             desDir = join("./log", curExpName)
             print(json.dumps({curExpName:1}, indent=4))
             setStdoutToDefault(f)
