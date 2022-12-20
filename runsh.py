6a283c69e7fa36649f8b7fac736a74804d32bc66
mary <dsa66253@yahoo.com.tw>
2022-12-20 22:12:01 +0800

    1218.brutL0L1

--- runsh.py
+++ runsh.py
@@ -38,35 +38,21 @@ def doExpBasedExperiments():
 def brutNas():
     # this funciion also handle decode job
     initiManualAssign = {
-        "layer_0_1": [
-            1,
-            0,
-            0,
-            0,
-            0
-        ],
-        "layer_1_2": [
-            1,
-            0,
-            0,
-            0,
-            0
-        ],
-        # "layer_2_3": [
+        # "layer_0_1": [
         #     1,
         #     0,
         #     0,
         #     0,
         #     0
         # ],
-        # "layer_3_4": [
+        # "layer_1_2": [
         #     1,
         #     0,
         #     0,
         #     0,
         #     0
         # ],
-        "layer_2_4": [
+        "layer_0_4": [
             0,
             0,
             0,
@@ -83,16 +69,16 @@ def brutNas():
     }
     # brutally train all possible arch of first two layers
 
-    for i in range(5):
+    for i in range(4, -1, -1):
         # for fisrt layer
-        for j in range(5):
+        for j in range(4, -1, -1):
             # for second layeer
             manualAssign = copy.deepcopy(initiManualAssign)
             
-            manualAssign["layer_2_4"][i] = 1
+            manualAssign["layer_0_4"][i] = 1
             manualAssign["layer_4_5"][j] = 1
             f = setStdoutToFile("./curExperiment.json")
-            curExpName = "1216.brutL2L3.{}_{}".format(i, j)
+            curExpName = "1218.brutL0L1.{}_{}".format(i, j)
             desDir = join("./log", curExpName)
             print(json.dumps({curExpName:1}, indent=4))
             setStdoutToDefault(f)
